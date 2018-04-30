7966c5fa46f720eb8f1e3922034a5a6ccc32928e
clarkyu <clarkyu@vip.qq.com>
2018-04-30 11:24:35 +0800

    profile every req.

--- config/config.default.php
+++ config/config.default.php
@@ -3,17 +3,17 @@
  * Default configuration for Xhgui
  */
 return array(
-    'debug' => false,
+    'debug' => true,
     'mode' => 'development',
 
     // Can be either mongodb or file.
-    /* 
+    /*
     'save.handler' => 'file',
     'save.handler.filename' => dirname(__DIR__) . '/cache/' . 'xhgui.data.' . microtime(true) . '_' . substr(md5($url), 0, 6),
     */
     'save.handler' => 'mongodb',
 
-    // Needed for file save handler. Beware of file locking. You can adujst this file path 
+    // Needed for file save handler. Beware of file locking. You can adujst this file path
     // to reduce locking problems (eg uniqid, time ...)
     //'save.handler.filename' => __DIR__.'/../data/xhgui_'.date('Ymd').'.dat',
     'db.host' => 'mongodb://127.0.0.1:27017',
@@ -30,7 +30,8 @@ return array(
     // Profile 1 in 100 requests.
     // You can return true to profile every request.
     'profiler.enable' => function() {
-        return rand(1, 100) === 42;
+        return true;
+        // return rand(1, 100) === 42;
     },
 
     'profiler.simple_url' => function($url) {
